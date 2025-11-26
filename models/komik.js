51edd98669e42f5fdeb2a1dda011879e427847a6
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-11-26 11:33:42 +0700

    Ganti file komik.js

--- models/komik.js
+++ models/komik.js
@@ -4,16 +4,10 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Komik extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index.js` file will call this method automatically.
-     */
     static associate(models) {
-      // RELASI: Komik itu MILIK satu User (Belongs To)
       Komik.belongsTo(models.User, {
-        foreignKey: 'userId', // Nyambung ke User lewat kolom ini
-        as: 'pemilik'         // Nanti pas dipanggil namanya 'pemilik'
+        foreignKey: 'userId', 
+        as: 'pemilik'         
       });
     }
   }
@@ -22,10 +16,9 @@ module.exports = (sequelize, DataTypes) => {
     penulis: DataTypes.STRING,
     deskripsi: DataTypes.TEXT,
     
-    // INI KOLOM PENTING (Foreign Key)
     userId: {
       type: DataTypes.INTEGER,
-      allowNull: false, // Komik gaboleh yatim piatu (harus ada user yg bikin)
+      allowNull: false, 
       references: {
         model: 'users',
         key: 'id'
@@ -34,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Komik',
-    tableName: 'komik' // Pastikan nama tabelnya 'komik' (huruf kecil)
+    tableName: 'komik' 
   });
   return Komik;
 };
